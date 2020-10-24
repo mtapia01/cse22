4396f2b62bad970b305a2869db6f327f97181f83
Michael Tapia <michaeltapia80@yahoo.com>
2020-10-24 04:59:36 +0000

    init comit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__, static_folder = 'static', template_folder = 'templates')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 CORS(app)
 
+import random
+hangman_words = ['Halloween','Computer','Python', 'Website']
 # ===============================================================================
 
 # Render Home Page
@@ -12,9 +14,125 @@ CORS(app)
 def home():
     return render_template('index.html')
 
+@app.route('/artist_one')
+def artist_one():
+    return render_template('artist_one.html')
+
+@app.route('/artist_two')
+def artist_two():
+    return render_template('artist_two.html')
+
+@app.route('/artist_three')
+def artist_three():
+    return render_template('artist_three.html')
+
+@app.route('/artist_four')
+def artist_four():
+	return render_template('artist_four.html')
+
+@app.route('/discussion_Board')
+def discussion_Board():
+	return render_template('discussion_Board.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+
+@app.route('/verify')
+def verify():
+	username= request.args.get("username")
+	password= request.args.get("password")
+
+	if username == "Admin" and password == "im-In":
+		return "You have successfully signed in"
+	else:
+		return "Try Again"
+
+@app.route('/boardroom')
+def boardroom():
+	html_content = "<h1>Discussion Board</h1>"
+	return html_content
+	if username == "Admin" and password == "im-In":
+		return "You have successfully signed in"
+	else:
+		return "Try Again"
+	
+@app.route('/word')
+def word():
+	pos = random.randint(0,len(hangman_words)-1)
+	word = (hangman_words[pos])
+
+	return {'word': word, 'length': len(word)}
+
+@app.route('/word_lists')
+def word_lists():
+	user_guess = request.args.get("user_textbox").upper()
+	word_guesed = request.args.get("word_guessed").upper()
+	position = []
+	for i in range(len(word)):
+		if user_guess == word[i]:
+			position.append(i)
+	if len(position) == 0:
+		return "Try Again"
+	return {'user_guess': user_guess, "word": word, "position": position}
+
+@app.route('/check_attempt')
+def check_attempt():
+	letter = request.args.get('letter')
+	position = int(request.args.get('index'))
+
+
+	# right_letter_zero = ['H','a','l','o','w','e','n']
+	# right_letter_one = ['C','o','m','p','u','t','e','r']
+	# right_letter_two = ['P','y','t','h','o','n']
+	# right_letter_three = ['W','e','b','s','i','t','e']
+	# right_letters_all = [right_letter_one + right_letter_two + right_letter_zero]
+
+	# position = []
+	
+	# for right_letter_zero in range(len(hangman_words)):
+	# 	if hangman_words[''] == right_letter_zero:
+	# 			position.append(right_letter_zero)
+	# 	else:
+	# 		return "Try Again"
+
+	# for right_letter_one in range(len(hangman_words)):
+	# 	if hangman_words[''] == right_letter_one:
+	# 			position.append(right_letter_one)
+	# 	else:
+	# 		return "Try Again"
+
+	# for right_letter_two in range(len(hangman_words)):
+	# 	if hangman_words[''] == right_letter_two:
+	# 		position.append(right_letter_two)
+	# 	else:
+	# 		return 'Try Again'
+
+	# return {'right_letters_all': right_letters_all, 'position': position}
+
+
+#This is not needed anymore. This was lab 4
+# @app.route('/game_check')
+# def game_check():
+# 	user_guess = request.args.get("user_guess").upper()
+# #if a letter is right it will say good job if it is not right it will say try again
+# 	if user_guess == "C":
+# 		return "C"
+# 	elif user_guess == "M":
+# 		return "M"
+# 	elif user_guess == "O":
+# 		return "O"
+# 	elif user_guess == "U":
+# 		return "U"
+# 	elif user_guess == "R":
+# 		return "R"
+	
+# 	else:
+# 		return "Try Again"
 
 
 
-# ===============================================================================
+#=======================================================
 if __name__ == '__main__':
-        app.run(debug=True, host="0.0.0.0", port=9000)
+        app.run(debug=True, host='0.0.0.0', port=9000)
--- static/main.js
+++ static/main.js
@@ -1,3 +1,210 @@
-$(document).ready(function () {
+wordIndex = -1
 
+$(document).ready(function (){
+	// $("#gamebtn").click(function(){
+	// })
+	$("#mybtn").click(function(){
+		let name = $("#inputbox").val();
+		let nametwo = $("#musicresponses").val();
+		$("#responses").html("<p>"+ name + ":" + " " + nametwo + "</p>")
+								})
+
+$("#sign-In").click(function() {
+	let userName = $("#username").val();
+	let passWord = $("#password").val();
+	$.get('/verify', {username: userName, password: passWord}, function(response){
+		if(response == "You have successfully signed in"){
+
+			$.get('/boardroom', {}, function(signed_response){
+					$("#boardroom").html(signed_response);
+					$("#login-space").addClass("hidden");
+					$("#board").removeClass("hidden");
+					$("#posted-Comments").removeClass("hidden");
+				})											
+				}
+		else{
+			alert("Try Again")
+			}
+		})
+	})
+
+
+
+	var array = ["Halloween", "Computer", "Python", "Website"]
+
+	let word = ""
+	$.get('/word', {}, function(response){
+		let wordLength = response['word']
+		wordIndex = response['length']
+		for (let i=0; i < wordIndex; i++){
+			$("#jumbo").append(`<span id= "letter${i}">_ </span>`);
+		}
+	});
+wrong = 0;
+	//var user_guess = document.getElementById("user_textbox").value 
+	$("#user_textbox").keypress(function(){
+		let value = $("#user_textbox").val();
+		if (value.length > 0){
+			$("#letter_box").val("");
+		}
+	 	//)}
+	});
+
+	
+	$("#gamebtn").click(function(){
+		let guess = $("#user_textbox").val();
+
+		$.get('/word_lists', {guess: guess}, function(response){
+			let letter = response["letter"]
+			let positions = response["positions"]
+			let spot = "#letter" + positions
+				
+			$(spot).html(letter);
+			
+
+			if (positions < 0)
+				wrong = wrong +1;
+				wrong = wrong +1
+				for (let i=0; i < positions.length; i++){
+					let item = '#word' + positions[i];
+					$(item).html(word);
+			
+				
+				 
+				if (response == "Try Again") {
+					wrong += 1
+				}
+				if (wrong == 1) {
+					$("#head").removeClass("hidden");
+				}
+				if (wrong == 2) {
+					$("#body").removeClass("hidden");
+				}
+				if (wrong == 3) {
+					$("#left-arm").removeClass("hidden");
+				}
+				if (wrong == 4) {
+					$("#right-arm").removeClass("hidden");
+				}
+				if (wrong == 5) {
+					$("#right-leg").removeClass("hidden");
+				}
+				if (wrong == 6) {
+					$("#left-leg").removeClass("hidden");
+					$("#gameover").removeClass("hidden");
+				}
+			}
+			})
+		});
+	//});
+
+	
+	// 	let value_one = $("#hangman_words").val
+	// 	if (value_one.length > 0){
+	// 		$("#hangman_words").val("");
+	// 	}
+	// })
+	
+	$("#gamebtn").click(function(){
+		// This is getting all the right letters and storing them so they can be checked when an answer is submitted. THIS DOES NOT WORK
+		$.get('/word_lists', {}, function(response){
+			let word = response['word']
+			let positions = response['positions']
+		// This is checking to see if an answer matches with the postion of a letter from my list 
+			for (let i=0; i < positions.length; i++){
+				let item = '#word' + positions[i];
+				$(item).html(word);
+		}
+		})
+	})
+	
+		$("#gamebtn").click(function(){
+		$.get('/check_attempt', {'letter': letter, 'index': wordIndex}, function(response){
+			let letter = response['letter']
+			let positions = response['positions']
+
+			for (let i=0; i < positions.length; i++){
+				let item = '#letter' + positions[i];
+				$(item).html(letter);
+			}
+
+		})
+	});
+		// let wrong = 0 	
+	$("#user_textbox").keypress(function(){
+		let value = $("#user_textbox").val();
+		if (value.length > 0){
+			$("#user_textbox").val("");
+		}
+	})
+	// This assigns the number of _ a word needs. Works as of Oct 22.
+	$('/word_lengths',{},function(response){
+		position = response['postion']
+		length = response['length']
+		var i; 
+		for (let i = 0; i < length; i++){
+		$("#jumbo").append('<span id= "letter${i}">_</span>')	
+		}
+	})
+
+
+	// $("#gamebtn").click(function(){
+	// 	alert("hello") 
+	// let user_guess = $("#user_guess").val();
+	// })
+	// $("#user_guess").val("");
+	// $.get('/word_lists', {user_guess: user_guess}, function(response){
+	// 		// Lab 4 stuff
+	// 		// if (response != "Try Again")
+	// 		// 	if	(response == "C"){
+	// 		// 		$("#letter_c").addClass("hidden")
+	// 		// 		$("#letter_one").removeClass("hidden");
+	// 		// 	}
+
+	// 	})
+
+	// wrong should add the body parts
+	//gobal variable
+
+	// let wrong = 0 	
+	// $("#user_guess").keypress(function(){
+	// 	let value = $("#user_guess").val();
+	// 	if (value.length > 0){
+	// 		$("#user_guess").val("");
+	// 	}
+	// })
+	// $("#gamebtn").click(function(){
+	// 	let user_guess = $("#user_guess").val();
+	// 	if (value.length > 0){
+	// 		$("#user_guess").val("");
+	// 	}
+	// })
+	// this chekcs to see if the letter is correct
+	//	$("#user_guess").val("");
+
+	// 	$.get('/game_check', {user_guess: user_guess}, function(response){
+	// 		if (response != "Try Again"){
+	// 			if	(response == "C"){
+	// 				$("#letter_c").addClass("hidden")
+	// 				$("#letter_one").removeClass("hidden");
+	// 			}
+	// 			if (response == "O"){
+	// 				$("#letter_o").addClass("hidden")
+	// 				$("#letter_two").removeClass("hidden");
+	// 			}
+	// 			if (response == "M"){
+	// 				$("#letter_m").addClass("hidden")
+	// 				 $("#letter_three").removeClass("hidden");
+	// 			}	
+	// 			if (response == "U"){
+	// 				$("#letter_u").addClass("hidden")
+	// 				$("#letter_four").removeClass("hidden");
+	// 			}	
+	// 			if (response == "R"){
+	// 				$("#letter_r").addClass("hidden")
+	// 				$("#letter_five").removeClass("hidden");
+	// 			}
+				
+				
+	 		
 });
